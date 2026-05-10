2228452e73a150033a868fa8d526460f56144c99
Wandersonrp <wandersonrp.49@hotmail.com>
2026-05-10 11:40:58 -0300

    feat(mobile): adiciona projeto inicial Ecocell.Mobile (MAUI Blazor)

---
+++ src/Ecocell.Mobile/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecocell.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "Ecocell.Mobile" };
+    }
+}
